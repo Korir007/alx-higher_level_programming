7d358a0f59f4d820eb257507ea8b67c67e763e98
Korir007 <danielkorir303@gmail.com>
2023-05-11 13:54:03 +0300

    minor fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 
-if __name__ == "__main__":
+if __name__ == "__main__" :
     """Performs varied calculations like multiplication, addtion, subtraction"""
     from calculator_1 import add, sub, mul, div
 
