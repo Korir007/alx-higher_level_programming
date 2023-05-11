d206bb139cfba415b611c0ef4e7af9e03b9922a3
Korir007 <danielkorir303@gmail.com>
2023-05-11 04:37:20 +0300

    task 9

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
-for dgt1 in range(0, 9):
+for dgt1 in range(0, 10):
     for dgt2 in range(dgt1 + 1, 10):
-        if dgt1 == 8:
-            print("{:d}{:d}".format(dgt1, dgt2))
+        if dgt1 == 8 and dgt2 == 9:
+            print("{}{}".format(dgt1, dgt2))
             break
 
-    print("{:d}{:d}".format(dgt1, dgt2), end=", ")
+    print("{}{}".format(dgt1, dgt2), end=", ")
